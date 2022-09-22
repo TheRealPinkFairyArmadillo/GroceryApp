aa87d302eb6ddf994bc5fc6474d4e031e2784bc7
TrineMedina <trinefirestone@gmail.com>
2022-09-22 17:54:12 -0400

    started working on list

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 // import './index.css'; //update stylesheets and locations?
 import './stylesheets/Recipe.css'
 import './stylesheets/RecipeSearch.css'
+import './stylesheets/List.css'
+import './stylesheets/Mobile.css'
 import App from './NewApp';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
