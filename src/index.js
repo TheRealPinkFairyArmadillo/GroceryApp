82bbc8a4742d992d0feaecbb41b9c7c628ac82af
TrineMedina <trinefirestone@gmail.com>
2022-09-22 17:09:16 -0400

    styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css'; //update stylesheets and locations?
+// import './index.css'; //update stylesheets and locations?
+import './stylesheets/Recipe.css'
+import './stylesheets/RecipeSearch.css'
 import App from './NewApp';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
